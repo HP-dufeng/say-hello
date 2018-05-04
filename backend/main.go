36c38d78b5989b9e11833b90f1dc59bf4c00a536
feng.du <fengdu_xiangyang@163.com>
2018-05-04 17:16:23 +0800

    change backend

---
+++ backend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
