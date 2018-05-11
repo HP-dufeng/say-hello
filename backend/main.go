c1165fdc4cd3ad3984234652bc91278274ab9eb5
feng.du <fengdu_xiangyang@163.com>
2018-05-11 14:46:06 +0800

    add client.go

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,63 @@
 package main
 
 import (
-	"log"
-	"os"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
 	"os/exec"
+
+	pb "github.com/fengdu/say-hello/api"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
+// func main() {
+// 	cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
+// 	if err := cmd.Run(); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
 func main() {
-	cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+	port := flag.Int("p", 8080, "port to listen to")
+	flag.Parse()
+
+	log.Infof("listening to port %d", *port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("could not listen to port %d: %v", *port, err)
 	}
+
+	s := grpc.NewServer()
+	pb.RegisterTextToSpeechServer(s, server{})
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("could not serve: %v", err)
+	}
+}
+
+type server struct{}
+
+func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, fmt.Errorf("could not create tmp file: %v", err)
+	}
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("could not close %s: %v", f.Name(), err)
+	}
+
+	cmd := exec.Command("flite", "-t", text.Text, "-o", f.Name())
+	if data, err := cmd.CombinedOutput(); err != nil {
+		return nil, fmt.Errorf("flite failed: %s", data)
+	}
+
+	data, err := ioutil.ReadFile(f.Name())
+	if err != nil {
+		return nil, fmt.Errorf("could not read tmp file: %v", err)
+	}
+
+	return &pb.Speech{Audio: data}, nil
 }
